b00d79f6fa53e9b5e845a1db1a8ac4a1aef1fe69
siegfried9516 <madsilveratrest@protonmail.com>
2024-04-14 05:34:20 +0800

    first commit

---
+++ autoclick_age.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         動畫瘋自動點擊年齡確認
+// @namespace    https://github.com/siegfried9516/autoclick_age
+// @version      0.3
+// @description  自動點擊巴哈姆特動畫瘋的年齡確定按鈕
+// @author       siegfried9516
+// @match        https://ani.gamer.com.tw/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var confirmButtonSelector = "#adult";
+
+    var checkExist = setInterval(function() {
+        var confirmButton = document.querySelector(confirmButtonSelector);
+        if (confirmButton) {
+            confirmButton.click();
+            clearInterval(checkExist);
+        }
+    }, 500); // 每500毫秒檢查一次按鈕是否存在
+})();
