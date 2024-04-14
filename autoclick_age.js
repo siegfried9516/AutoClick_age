614c6c3b9321fcdc78629c1b7656bb602618f51b
siegfried9516 <madsilveratrest@protonmail.com>
2024-04-14 22:53:08 +0800

    use MutationObserver

--- autoclick_age.js
+++ autoclick_age.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         動畫瘋自動點擊年齡確認
 // @namespace    https://github.com/siegfried9516/autoclick_age
-// @version      0.3
+// @version      0.4
 // @description  自動點擊巴哈姆特動畫瘋的年齡確定按鈕
 // @author       siegfried9516
-// @match        https://ani.gamer.com.tw/*
+// @match        https://ani.gamer.com.tw/animeVideo.php*
 // @grant        none
 // ==/UserScript==
 
@@ -13,8 +13,10 @@
 
     var confirmButtonSelector = "#adult";
 
-    var checkExist = setInterval(function() {
+    var observer = new MutationObserver(function(mutations) {
         var confirmButton = document.querySelector(confirmButtonSelector);
         if (confirmButton){ confirmButton.click(); }
-    }, 500); // 每500毫秒檢查一次按鈕是否存在
+    });
+
+    observer.observe(document, { childList: true, subtree: true });
 })();
